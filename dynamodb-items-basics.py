b17485f271d8f4f977abc49a36814e511dcd3239
EshitaMehta <itiseshita@gmail.com>
2023-04-28 16:13:57 +0530

    d

---
+++ dynamodb-items-basics.py
@@ -0,0 +1,35 @@
+import boto3
+
+# Table Name 
+table_name = 'Movies'
+
+# dynamodb client 
+dynamodb_client = boto3.client('dynamodb')
+
+# item_scarface 
+item_scarface  = {
+    'Title': {'S': 'Dipesh'}
+    ,'Year': {'S': '2001'}
+}
+
+ # get item
+"""
+item_get ={
+    'Title': {'S':'Scarface'}
+    ,'Year': {'S': '1983'}
+}
+"""
+
+if __name__ == "__main__":
+
+    # put item into db
+    dynamodb_client.put_item(TableName = table_name, Item= item_scarface)
+
+    
+    # get item
+    """
+    resp = dynamodb_client.get_item(TableName = table_name, Key = item_get)
+    print(resp)
+    print(resp['Item'])
+    print(resp['Item']['Title']['S'])
+    """
